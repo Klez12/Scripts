6fbd80c46c74874c04698e02835943f1c41b710d
komal <kkumari97531@gmail.com>
2025-05-24 07:52:16 +0530

    host

--- app.py
+++ app.py
@@ -26,5 +26,9 @@ def ask():
     answer = response.choices[0].message.content
     return jsonify({"answer": answer})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
